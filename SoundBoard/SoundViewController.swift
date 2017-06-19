2b62b5446bc7ae01bac94ac7062a649faa685bc8
Tobias 托比 Gattermayer 戴 <vucoet@gmail.com>
2017-06-19 15:52:49 +0800

    View Setup!

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by vux on 19/6/17.
+//  Copyright © 2017 crypticmantra. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBAction func recordButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func playButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func addButtonAction(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
